1505da0a3c146a0c6b4ecbb93a5e5dc128070446
Giuliano Crenna <59456310+giulicrenna@users.noreply.github.com>
2023-06-12 17:38:28 -0300

    Installer updated

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-clear
-
-python3 main.py yolov5mu.pt
-
-exit 0
--- setup.sh
+++ setup.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+mkdir /etc/darkflow_weed
+
+cp -r ./ /etc/darkflow_weed
+
+cp /etc/darkflow_weed/darkflow_weed /bin/
+
+chmod +x /bin/darkflow_weed
+
+cd /etc/darkflow_weed
+
 echo "INSTALANDO DEPENDENCIAS"
+
 pip install -r requirements.txt
 
